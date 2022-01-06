f05222a3bbafba7dd672aa30ebc54019b755143a
MenD <menpro147@gmail.com>
2022-01-07 06:39:11 +0700

    [Web] Đăng ký thí sinh

--- WebTrungTamNgoaiNgu/App_Start/RouteConfig.cs
+++ WebTrungTamNgoaiNgu/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebTrungTamNgoaiNgu
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "DangKyThi", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ WebTrungTamNgoaiNgu/Controllers/ThongKeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebTrungTamNgoaiNgu.Controllers
+{
+    public class ThongKeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
