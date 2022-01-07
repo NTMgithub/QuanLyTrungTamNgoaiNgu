5ccbc85a849a0ddc295f0065b0ec6a40f1a99d3e
MenD <menpro147@gmail.com>
2022-01-07 14:11:35 +0700

    [Web] Tra cứu giấy chứng nhận kết quả

--- WebTrungTamNgoaiNgu/Controllers/TraCuuGiayChungNhanController.cs
+++ WebTrungTamNgoaiNgu/Controllers/TraCuuGiayChungNhanController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DAL;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +10,31 @@ namespace WebTrungTamNgoaiNgu.Controllers
 {
     public class TraCuuGiayChungNhanController : Controller
     {
+        D_KhoaThi d_KhoaThi = new D_KhoaThi();
+        D_DSThiSinhTrongPhongThi d_DSTSphongThi = new D_DSThiSinhTrongPhongThi();
         public ActionResult Index()
         {
             return View();
         }
 
-       
+        [HttpGet]
+        [Route("GetAllKhoaThi")]
+        public JsonResult GetAllKhoaThi()
+        {
+            
+            var getList = d_KhoaThi.GetAllKhoaThi();
+            return Json(getList, JsonRequestBehavior.AllowGet);
+        }
+        
+
+        [HttpGet]
+        [Route("GetThongTinGiayChungNhan")]
+        public JsonResult GetThongTinGiayChungNhan(int maKhoaThi, String soBaoDanh)
+        {
+
+            var getList = d_DSTSphongThi.GetThongTinGiayChungNhan(maKhoaThi, soBaoDanh);
+            return Json(getList, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
